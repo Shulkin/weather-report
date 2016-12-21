db82903d2408c2aaec2ea3027b632cc6024c839e
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-12-21 13:15:17 +1000

    Set map center

--- public/src/js/controllers/map-ctrl.js
+++ public/src/js/controllers/map-ctrl.js
@@ -1,7 +1,17 @@
 angular.module("map.ctrl", ["openlayers-directive"])
-.controller("MapCtrl", [function() {
+.controller("MapCtrl", ["WeatherData", function(WeatherData) {
   // === Variables ===
   var vm = this;
+  // default center of the map
+  vm.primorye = {
+    lat: 45.3,
+    lon: 134.6,
+    zoom: 6
+  };
+  // layers list
+  vm.layers = []; // empty
+  // markers on map
+  vm.markers = []; // empty
   // === Private ===
   // constructor
   function init() {
