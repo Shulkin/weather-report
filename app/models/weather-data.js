812f5548852a3fdf79e1f62784202e7ca6b0a286
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-12-28 11:44:01 +1000

    Define weather data schema on backend

--- app/models/weather-data.js
+++ app/models/weather-data.js
@@ -1,1 +1,24 @@
 var mongoose = require("mongoose");
+// define weather data schema
+var WeatherDataSchema = new mongoose.Schema({
+  // city name
+  name: {type: String, required: true},
+  // store JSON data from OWM API as String
+  data: {type: String, required: true},
+  // timestamps
+  created_at: {type: Date, default: Date.now},
+  updated_at: {type: Date, default: Date.now}
+}, {
+  // explicitly state the collection name
+  collection: "data"
+});
+// set created_at equal to the current time on save data
+WeatherDataSchema.pre("save", function(next) {
+  now = new Date();
+  this.updated_at = now;
+  if (!this.created_at) {
+    this.created_at = now;
+  }
+  next();
+});
+module.exports = mongoose.model("WeatherData", WeatherDataSchema);
--- app/routes/weather-routes.js
+++ app/routes/weather-routes.js
@@ -1,5 +1,8 @@
 var express = require("express");
 // use express built-in router
 var router = express.Router();
+// get mongoose model
+var WeatherData = require("../models/weather-data");
+// process api/weather
 // export routes
 module.exports = router;
